5fc69cc639892c16efaf4c72a5debc42b9f5cc6e
Aaron William Po <apo2@uwo.ca>
2022-11-18 21:12:59 -0500

    Update to env variable name

--- src/database/AppDataSource.ts
+++ src/database/AppDataSource.ts
@@ -1,12 +1,11 @@
 import { DataSource } from 'typeorm';
 import DiscordGuild from './model/DiscordGuild';
 
-const { LOCAL_DB_CONNECTION_STRING } = process.env;
+const { LOCAL_DB_CONNECTION_STRING: DB_CONNECTION_STRING } = process.env;
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  url: LOCAL_DB_CONNECTION_STRING,
-  synchronize: true,
+  url: DB_CONNECTION_STRING,
   entities: [DiscordGuild],
 });
 
