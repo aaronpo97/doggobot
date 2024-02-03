3e2a1e540d36846f1994f373c2e2643d9126b4dc
Aaron William Po <apo2@uwo.ca>
2024-02-02 19:28:41 -0500

    Refactor: begin repo cleanup

--- src/scheduled-jobs/getRandomPost.ts
+++ src/scheduled-jobs/getRandomPost.ts
@@ -2,8 +2,8 @@ import PostSchema from '../util/zod/PostSchema';
 
 const getRandomPost = async () => {
   const response = await fetch('https://dog.ceo/api/breeds/image/random');
-  const data = await response.json();
-  const parsed = PostSchema.safeParse(data);
+  const json = await response.json();
+  const parsed = PostSchema.safeParse(json);
 
   if (!parsed.success) {
     throw new Error('Failed to parse response from API');
